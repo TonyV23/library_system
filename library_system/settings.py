4deae83f25ae6741e9e603e020094e6884d9b574
viper23 <htonnycarlos@gmail.com>
2022-12-08 12:35:24 +0200

    db config

--- library_system/settings.py
+++ library_system/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'library_system.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'library_db',
+        'USER':'root',
+        'PASSWORD':'',
+        'HOST':'localhost',
+        'PORT':'3306'
     }
 }
 
