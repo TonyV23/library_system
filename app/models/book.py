28fb827cb803721ca9eb9b90b1d959e9e08a3f9a
sgenlecroyant <sgenlecroyant@gmail.com>
2022-12-12 10:48:20 +0200

    added Book Model

---
+++ app/models/book.py
@@ -0,0 +1,10 @@
+from django.db import models
+from app.models import Author, Category
+
+class Book(models.Model):
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    book_title = models.CharField(max_length=30)
+    book_language = models.CharField(max_length=10)
+    edition_data = models.DateField(auto_created=False, auto_now=False)
+    
