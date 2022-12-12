1023a481f7571b8339612a7f614f1508346d4187
sgenlecroyant <sgenlecroyant@gmail.com>
2022-12-12 10:53:29 +0200

    defined str magic function

--- app/models/borrow.py
+++ app/models/borrow.py
@@ -6,3 +6,6 @@ class Borrow(models.Model):
     book = models.ForeignKey(Book, on_delete=models.CASCADE)
     borrow_starting_date = models.DateField(auto_created=False, auto_now=False)
     borrow_ending_date = models.DateField(auto_created=False, auto_now=False)
+
+    def __str__(self) -> str:
+        return self.borrower+ " " +self.book
