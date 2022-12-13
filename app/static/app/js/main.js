70c4dead15318fa10707521594f66f8de70c5f0a
StarBot1 <hamzadushime@gmail.com>
2022-12-13 15:30:31 +0200

    add main.js

---
+++ app/static/app/js/main.js
@@ -0,0 +1,16 @@
+// Getproduct by category_id
+$(function() {
+    $('#category_id').on("change", function () {
+        category_id = $(this).val();
+        //alert(category_id);
+        $.get(
+            "/borrows/getBooks",
+            {
+                category_id: category_id,
+            },
+            function (data, textStatus, jqXHR) {
+                $("#id_book").html(data);
+            }
+        );
+    } );
+});
