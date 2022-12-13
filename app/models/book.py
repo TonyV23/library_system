687c7c70bff2e5580e6329a2ff4c830d2643b87b
viper23 <htonnycarlos@gmail.com>
2022-12-13 17:59:41 +0200

    changed date format

--- app/models/book.py
+++ app/models/book.py
@@ -1,12 +1,13 @@
 from django.db import models
 from app.models import Author, Category
+from library_system import settings
 
 class Book(models.Model):
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     book_title = models.CharField(max_length=30)
     book_language = models.CharField(max_length=10)
-    edition_date = models.DateField(auto_created=False, auto_now=False)
+    edition_date = models.DateField(settings.DATE_INPUT_FORMATS,auto_created=False, auto_now=False)
     
 
     def __str__(self) -> str:
