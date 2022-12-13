52fd9ae4b7fac709618d7541167950a51389f147
StarBot1 <hamzadushime@gmail.com>
2022-12-13 15:26:56 +0200

    add from .borrow_form import BorrowForm

--- app/forms/__init__.py
+++ app/forms/__init__.py
@@ -2,3 +2,4 @@ from .author_form import AuthorForm
 from .category_form import CategoryForm
 from .book_form import BookForm
 from .borrower_form import BorrowerForm
+from .borrow_form import BorrowForm
